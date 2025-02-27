fc1f1d349a4e95902d2b0562cd7fcd8c2417f352
PalOntEr <105398631+PalOntEr@users.noreply.github.com>
2025-02-26 19:35:15 -0600

    Cleanup: Remove unused using directives across files
    This commit removes several unnecessary `using` directives from `Filter.cs`, `CameraEdit.cs`, `ImageEdit.cs`, and `VideoEdit.cs`. The following namespaces were removed: `System.Collections.Generic`, `System.ComponentModel`, `System.Data`, `System.Linq`, `System.Runtime.InteropServices`, `System.Text`, `System.Threading`, and others. This cleanup enhances code readability and maintainability.

--- PhotoHub/Forms/CameraEdit.cs
+++ PhotoHub/Forms/CameraEdit.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
 using PhotoHub.Filters;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace PhotoHub
 {
